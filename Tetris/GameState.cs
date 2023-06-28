ec560c9f0078974311d39f0041aa3087a5df225c
Андрей Рочняк <ya1andrey@inbox.ru>
2023-06-28 19:25:42 +0300

    готовченко

--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -14,18 +14,34 @@ namespace Tetris
             {
                 currentBlock = value;
                 currentBlock.Reset();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    currentBlock.Move(1,0);
+
+                    if(!BlockFits())
+                    {
+                        currentBlock.Move(-1, 0);
+                    }
+                }
             }
         }
 
         public GameGrid GameGrid { get; }
         public BlockQueue BlockQueue { get; }
         public bool GameOver { get; private set; }
+        public int Score { get; private set; }
+        public Block HeldBlock { get; private set; }
+        public bool CanHold { get; private set; }
+
+
 
         public GameState()
         {
             GameGrid = new GameGrid(22, 10);
             BlockQueue = new BlockQueue();
             CurrentBlock = BlockQueue.GetAndUpdate();
+            CanHold = true;
         }
 
         private bool BlockFits()
@@ -38,6 +54,29 @@ namespace Tetris
                 }
             }
             return true;
+        }
+
+        public void HoldBlock()
+        {
+            if(!CanHold)
+            {
+                return;
+            }
+
+            if(HeldBlock == null)
+            {
+                HeldBlock = CurrentBlock;
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+            else
+            {
+                Block tmp = CurrentBlock;
+                CurrentBlock = HeldBlock;
+                HeldBlock = tmp;
+            }
+
+            CanHold = false;
+
         }
 
         public void RotateBlockCW()
@@ -88,7 +127,7 @@ namespace Tetris
                 GameGrid[p.Row, p.Column] = CurrentBlock.Id;
             }
 
-            GameGrid.ClearFullRows();
+           Score += GameGrid.ClearFullRows();
 
             if (IsGameOver())
             {
@@ -97,6 +136,7 @@ namespace Tetris
             else
             {
                 CurrentBlock = BlockQueue.GetAndUpdate();
+                CanHold = true;
             }
         }
 
@@ -109,6 +149,34 @@ namespace Tetris
                 CurrentBlock.Move(-1, 0);
                 PlaceBlock();
             }
+        }
+
+        private int TileDropDistance(Position p)
+        {
+            int drop = 0;
+
+            while (GameGrid.IsEmpty(p.Row + drop +1, p.Column))
+            {
+                drop++;
+            }
+
+            return drop;
+        }
+
+        public int BlockDropDistance()
+        {
+            int drop = GameGrid.Rows;
+            foreach (Position p in CurrentBlock.TilePosition())
+            {
+                drop = System.Math.Min(drop, TileDropDistance(p));
+            }
+            return drop;
+        }
+
+        public void DropBlock()
+        {
+            CurrentBlock.Move(BlockDropDistance(), 0);
+            PlaceBlock();
         }
     }
 }
--- Tetris/OBlock.cs
+++ Tetris/OBlock.cs
@@ -9,7 +9,7 @@ namespace Tetris
             new Position[]{new(0,0), new(0,1), new(1,0), new(1,1)}
         };
 
-        public override int Id => 2;
+        public override int Id => 4;
         protected override Position StartOffset => new Position(0,4);
         protected override Position[][] Tiles => tiles;
     }
